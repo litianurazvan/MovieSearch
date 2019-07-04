230cc89611f0882a3b242200c4fffcb92f813d21
Razvan Litianu <litianu.razvan@gmail.com>
2019-07-04 09:41:09 +0300

    Initial Commit

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by Razvan Litianu on 7/4/19.
+//  Copyright © 2019 Razvan Litianu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
